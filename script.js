6c3db244fffd1d2baadac161a38cdcdbc6f522f8
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-08-04 20:55:53 +1000

    Finalize pseudo code Decide to delcare SELECTIONS constant holds an array and take user input from prompt() function.

--- script.js
+++ script.js
@@ -16,47 +16,58 @@
 //  create "index" variable that random number between 0 - 2 is assigned when the function called
 //  return the value of the array in the location of "index".
 
-// create a function that decides the result of the round - playRound.
+// create a function that decides the result of the round - playRound().
 // Input: text(playerSelection, computerSelection), Output: boolean or null
-// IF player selected "rock"
-//  IF computer selected "rock"
-//    return null
-//  IF computer selected "paper"
-//    return false
-//  IF computer selected "scissors"
-//    return true
-// IF player selected "paper"
-//  IF computer selected "rock"
-//    return true
-//  IF computer selected "paper"
-//    return null
-//  IF computer selected "scissors"
-//    return false
-// IF player selected "scissors"
-//  IF computer selected "rock"
-//    return false
-//  IF computer selected "paper"
-//    return true
-//  IF computer selected "scissors"
-//    return null
+//  Create rock variable an assign the 1st element of the SELECTION array.
+//  Create paper variable an assign the 2nd element of the SELECTION array.
+//  Create scissors variable an assign the 3rd element of the SELECTION array.
+//  IF player selected "rock"
+//   IF computer selected "rock"
+//     return null
+//   IF computer selected "paper"
+//     return false
+//   IF computer selected "scissors"
+//     return true
+//  IF player selected "paper"
+//   IF computer selected "rock"
+//     return true
+//   IF computer selected "paper"
+//     return null
+//   IF computer selected "scissors"
+//     return false
+//  IF player selected "scissors"
+//   IF computer selected "rock"
+//     return false
+//   IF computer selected "paper"
+//     return true
+//   IF computer selected "scissors"
+//     return null
 
 // Create a function that call the "playRound" function 5 times - game().
-// Create "playerSelection" and assign text that is convert into lower text to it.
-// Create "computerSelection" and assign the result of "getComputerSelection" function.
-// Create "playerWon" and assign number 0
-// Create "computerWon" and assign number 0
-// Create "youWinMessage" and assign text, "You Win!"
-// Create "drawMessage" and assign text, "Draw"
-// Create "youLoseMessage" and assign text, "You Lose!"
-// Begin with "i" at 0 and add 1 to "i" on each pass until "i" is less than 5
-//  Create "result" variable and assign the returned value of "playRound"
-//  IF the result of the function is true, then count up the "playerWon" variable.
-//  IF the result of the function is false, then count up "computerWon" variable.
-//  IF the result of the function is null, then do nothing.
-//IF "playerWon" is greater than "computerWon" then return the "youWinMessage".
-//IF "computerWon" is greater than "playerWon" then return the "youLostMessage".
-//ELSE return "drawMessage".
+//  Create SELECTIONS variable and assign the array with "rock", "paper", "scissors".
+//
+//  Initialize isValid with false.
+//  Continue loop as long as isValid is false. Inside the loop, change the isValid true IF:
+//    Take input from console and assign text value to "playerSelection" variable.
+//    Re assign the value of the "playerSelection" variable with lower case letter alphabets.
+//    Check the playerSelection's value is valid selection
+//    IF playerSelection has valid value, change the isValid to true.
+//    ELSE display the message - <selection> is wrong selection. Please enter "Rock", "Paper" or "Scissors".
+//
+//   Create "computerSelection" and assign the result of "getComputerSelection" function.
+//  Create "playerWon" and assign number 0
+//  Create "computerWon" and assign number 0
+//  Create "youWinMessage" and assign text, "You Win!"
+//  Create "drawMessage" and assign text, "Draw"
+//  Create "youLoseMessage" and assign text, "You Lose!"
+//  Begin with "i" at 0 and add 1 to "i" on each pass until "i" is less than 5
+//    Create "result" variable and assign the returned value of "playRound"
+//    IF the result of the function is true, then count up the "playerWon" variable.
+//    IF the result of the function is false, then count up "computerWon" variable.
+//    IF the result of the function is null, then do nothing.
+//
+//  IF "playerWon" is greater than "computerWon" then return the "youWinMessage".
+//  IF "computerWon" is greater than "playerWon" then return the "youLostMessage".
+//  ELSE return "drawMessage".
 
-// take an input from console and store the value as playerSelection.
-// convert the input to the lowercase letter and trim white spaces if any.
 // call the game() function.
